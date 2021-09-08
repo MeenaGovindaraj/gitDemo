8cf74d5f586e8e542fa7a9d0a4b4c31c3eb6f6c8
Meena-Govindaraj <88269382+Meena-Govindaraj@users.noreply.github.com>
2021-09-08 11:02:43 +0530

    Commited By Meena

---
+++ src/main/java/com/revature/training/controller/GitDemoController.java
@@ -0,0 +1,10 @@
+package com.revature.training.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//to check push
+public class GitDemoController {
+
+	
+}
